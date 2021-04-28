ba9d391997adabc817cd3f1b4d40fe7d675c848c
1garo <alevardai@hotmail.com>
2021-04-28 00:59:23 -0300

    feat: call transaction func

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/1garo/go-eth/helpers"
+)
+
+func main() {
+	//TODO: command to generate smart contract to deploy it later
+	// abigen --sol=Store.sol --pkg=main --out=store.go
+	// go run main.go store.go
+	helpers.Ma()
+}
